92dad46e7ab2cc6048b4235ad48c5a78eef7bfce
Huseyin <huseyin.kann@hotmail.com>
2024-05-01 17:04:35 +0300

    Courses page initialez

--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,7 @@
 import { pgTable, serial, text } from "drizzle-orm/pg-core";
 
 export const courses = pgTable("courses",{
-    id: serial("id").notNull(),
+    id: serial("id").notNull().primaryKey(),
     title: text("title").notNull(),
     imageSrc: text("image_src").notNull(),
 })
