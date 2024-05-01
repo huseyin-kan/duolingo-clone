79a52e563632bcf33bc6c55ee0539d2c77f1f347
Huseyin <huseyin.kann@hotmail.com>
2024-05-01 14:23:11 +0300

    Learn page main

---
+++ app/(main)/learn/header.tsx
@@ -0,0 +1,24 @@
+import { Button } from '@/components/ui/button'
+import { ArrowLeft } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
+type Props = {
+    title: string
+  }
+  
+const Header = ({title}:Props) => {
+  return (
+    <div className='sticky top-0 bg-white pb-3 lg:pt-[28px] flex items-center justify-between mb-5 border-b-2 text-neutral-400 lg:z-50'>
+        <Link href="/courses">
+            <Button variant="ghost" size="sm">
+                <ArrowLeft className='size-5 text-neutral-400'/>
+            </Button>
+        </Link>
+        <h1 className='font-bold text-lg'>
+            {title}
+        </h1>
+    </div>
+  )
+}
+
+export default Header
--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,8 +1,24 @@
+import FeedWrapper from '@/components/feed-wrapper'
+import StickyWrapper from '@/components/sticky-wrapper'
 import React from 'react'
+import Header from './header'
+import UserProgress from '@/components/user-progress'
 
 const Learn = () => {
   return (
-    <div>Learn</div>
+    <div className='flex flex-row-reverse gap-[48px] px-6'>
+      <StickyWrapper>
+        <UserProgress 
+        hasActiveSubscription={false}
+        hearts={5}
+        activeCourse={{title:"Spanish",imageSrc:"/es.svg"}}
+        points={100}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title='Spanish'/>
+      </FeedWrapper>
+    </div>
   )
 }
 
