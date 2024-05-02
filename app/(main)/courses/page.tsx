b37eb4bb09918076efd9dcad1faaa67984a939e3
Huseyin <huseyin.kann@hotmail.com>
2024-05-02 14:53:09 +0300

    Courses fetched

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,8 +1,16 @@
+import { getCourses } from '@/db/queries'
 import React from 'react'
+import List from './list'
 
-const CoursesPage = () => {
+const CoursesPage = async () => {
+  const courses  = await getCourses()
   return (
-    <div>CoursesPage</div>
+    <div className='h-full max-w-[912px] px-3 mx-auto'>
+      <h1 className='text-2xl text-neutral-700 font-bold'>
+        Language Courses
+      </h1>
+      <List courses={courses} activeCourseId={1} />
+    </div>
   )
 }
 
