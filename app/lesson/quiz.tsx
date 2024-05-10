9cac4c7fbf967cdbf9116ca01789cedd3cef3796
Huseyin <huseyin.kann@hotmail.com>
2024-05-10 15:17:36 +0300

    Lesson Header Completed

---
+++ app/lesson/quiz.tsx
@@ -0,0 +1,38 @@
+"use client"
+import { challengeOptions, challenges } from '@/db/schema';
+import React, { useState } from 'react'
+import Header from './header';
+
+type Props = {
+    initialPercentage:number;
+    initialHearts:number;
+    initialLessonId:number;
+    initialLessonChallenges: (typeof challenges.$inferSelect & {
+        completed:boolean;
+        challengeOptions: typeof challengeOptions.$inferSelect[]
+    })[];
+    userSubscription:any
+}
+
+const Quiz = ({
+    initialHearts,
+    initialLessonId,
+    initialPercentage,
+    initialLessonChallenges,
+    userSubscription
+}:Props) => {
+    const [hearts,setHearts] = useState(initialHearts)
+    const [percentage,setPercentage] = useState( 50 || initialPercentage)
+
+  return (
+    <>
+        <Header
+            hearts={hearts}
+            percentage={percentage}
+            hasActiveSubscription={!!userSubscription?.isActive}
+        />
+    </>
+  )
+}
+
+export default Quiz
